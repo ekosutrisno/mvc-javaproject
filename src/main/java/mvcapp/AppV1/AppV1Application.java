000638bab9d55f469e5a29a85533b18ff6377178
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2019-12-06 20:44:09 +0700

    Load asset file

---
+++ src/main/java/mvcapp/AppV1/AppV1Application.java
@@ -0,0 +1,13 @@
+package mvcapp.AppV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppV1Application.class, args);
+	}
+
+}
