c57baea76b6b9f721e96fafd2f92ab94aca816b4
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2019-12-07 11:16:50 +0700

    Add config to run java

---
+++ src/main/java/mvcapp/AppV1/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package mvcapp.AppV1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * HomeController
+ */
+@Controller
+public class HomeController {
+
+  @GetMapping("/home")
+  public String viewHome() {
+    return "home";
+  }
+
+  @GetMapping("/index")
+  public String viewIndex() {
+    return "index";
+  }
+
+}
